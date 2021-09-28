2e5984f7af94b3969e0775a1e43d2c436d26612c
Julia <63058596+JuhGoveia@users.noreply.github.com>
2021-09-28 00:20:30 -0300

    Jogo super trunfo baseado no Naruto

---
+++ script.js
@@ -0,0 +1,377 @@
+//algumas imagens depois do teste deram bug...
+
+var cartaNaruto = {
+  nome: "Naruto",
+  imagem: "https://static1.purebreak.com.br/articles/2/14/48/2/@/74079-seria-naruto-o-ninja-mais-poderoso-de-diapo-1.jpg",
+  atributos: {
+    ataque: 25,
+    defesa: 18,
+    chakra: 29
+  }
+};
+
+var cartaSasuke = {
+  nome: "Sasuke",
+  imagem:    "https://i.pinimg.com/474x/6a/07/ba/6a07ba333669a2868959939bab9e7e11--sasuke-uchiha-naruto-shippuden.jpg",
+  atributos: {
+    ataque: 29,
+    defesa: 13,
+    chakra: 27
+  }
+};
+
+var cartaSakura = {
+  nome: "Sakura",
+  imagem: "https://pbs.twimg.com/media/DFrEPsgXgAEUYEf.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 2,
+    chakra: 18
+  }
+};
+
+var cartaLee = {
+  nome: "Rock Lee",
+  imagem:  "https://i.pinimg.com/originals/a0/4f/59/a04f5981a98f49408a4b3823fe7103a7.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 14,
+    chakra: 10
+  }
+};
+
+var cartaNeji = {
+  nome: "Neji",
+  imagem:    "hhttps://i.pinimg.com/originals/23/8e/ed/238eed569906aae44c21beabee832d1d.jpg",
+  atributos: {
+    ataque: 24,
+    defesa: 9,
+    chakra: 12
+  }
+};
+
+var cartaHinata = {
+  nome: "Hinata",
+  imagem: "https://i1.sndcdn.com/artworks-duy3Q2LFQKAgPIaX-cROJmg-t500x500.jpg",
+  atributos: {
+    ataque: 12,
+    defesa: 11,
+    chakra: 22
+  }
+};
+
+var cartaGaara = {
+  nome: "Gaara",
+  imagem:    "https://i.pinimg.com/originals/7c/bd/82/7cbd82f9c56772bc7efc1db64946a73c.jpg",
+  atributos: {
+    ataque: 12,
+    defesa: 13,
+    chakra: 22
+  }
+};
+
+var cartaTemari = {
+  nome: "Temari",
+  imagem:    "https://i.pinimg.com/originals/46/3f/59/463f59a5f54a0a91728d0df38fcf14d3.jpg",
+  atributos: {
+    ataque: 11,
+    defesa: 14,
+    chakra: 4
+  }
+};
+var cartaTenten = {
+  nome: "Tenten",
+  imagem:    "https://pm1.narvii.com/6297/afc86660b6133f9ddde059e2f2af5ab162a02bf4_hq.jpg",
+  atributos: {
+    ataque: 20,
+    defesa: 13,
+    chakra: 9
+  }
+};
+
+var cartaIno = {
+  nome: "Ino",
+  imagem:
+    "https://i.pinimg.com/236x/5e/54/f9/5e54f976c46edfe60bf00be72e34d0a2--queen-google-search.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 3,
+    chakra: 12
+  }
+};
+
+var cartaShikamaru = {
+  nome: "Shikamaru",
+  imagem:    "https://i.pinimg.com/originals/bf/c0/3c/bfc03c69b005cd80c9191dc5b631cf49.jpg",
+  atributos: {
+    ataque: 15,
+    defesa: 12,
+    chakra: 8
+  }
+};
+
+var cartaItachi = {
+  nome: "Itachi",
+  imagem:
+    "https://i.pinimg.com/736x/37/9a/af/379aafab0c2da1a06cd05e527d4cc28a.jpg",
+  atributos: {
+    ataque: 29,
+    defesa: 17,
+    chakra: 26
+  }
+};
+
+var cartaKakashi = {
+  nome: "Kakashi",
+  imagem:
+    "http://3.bp.blogspot.com/-2MgEZpRjJ7Q/T91bzR-tT1I/AAAAAAAABq0/P49YFB8FD_8/s2400/243068%242C020420111949.jpg",
+  atributos: {
+    ataque: 17,
+    defesa: 13,
+    chakra: 28
+  }
+};
+
+var cartaTsunade = {
+  nome: "Tsunade",
+  imagem:
+    "https://i.pinimg.com/1200x/62/63/24/62432442452bcace68178589934f213f.jpg",
+  atributos: {
+    ataque: 12,
+    defesa: 13,
+    chakra: 18
+  }
+};
+
+var cartaKisame = {
+  nome: "Kisame",
+  imagem:
+    "https://nerdhits.com.br/wp-content/uploads/2021/06/kisame-naruto-cosplay-1200x900.jpg",
+  atributos: {
+    ataque: 19,
+    defesa: 14,
+    chakra: 10
+  }
+};
+
+var cartaHidan = {
+  nome: "Hidan",
+  imagem:
+    "http://pm1.narvii.com/6483/99324fc8d813b95701e071ef584d6a3afd1edce0_00.jpg",
+  atributos: {
+    ataque: 13,
+    defesa: 9,
+    chakra: 7
+  }
+};
+
+var cartaDeidara = {
+  nome: "Deidara",
+  imagem:
+    "https://i.pinimg.com/222x/52/8b/f5/528bf5050e70c2b577aedf245579cc30.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 11,
+    chakra: 5
+  }
+};
+
+var cartaKakuso = {
+  nome: "Kakuso",
+  imagem:
+    "https://i.pinimg.com/474x/62/8d/77/628d7774cbd3f355e4b39b9fae63b6bd.jpg",
+  atributos: {
+    ataque: 11,
+    defesa: 13,
+    chakra: 9
+  }
+};
+
+var cartaKonan = {
+  nome: "Konan",
+  imagem:    "https://i.pinimg.com/originals/05/bf/c1/05bfc17da708f6da0282425e9b0af048.jpg",
+  atributos: {
+    ataque: 12,
+    defesa: 7,
+    chakra: 11
+  }
+};
+
+var cartaSasori = {
+  nome: "Sasori",
+  imagem:    "https://i.pinimg.com/originals/44/30/1f/44301f18c4e748add284d066f9d87210.jpg",
+  atributos: {
+    ataque: 11,
+    defesa: 6,
+    chakra: 4
+  }
+};
+
+var cartaOrochimaru = {
+  nome: "Orochimaru",
+  imagem:
+    "https://lh5.googleusercontent.com/-6xC6_B-qNH0/TYYJwBgy8BI/AAAAAAAAAEU/iwtqwqZxKPs/s2400/015_orochimaru.jpg",
+  atributos: {
+    ataque: 18,
+    defesa: 10,
+    chakra: 12
+  }
+};
+
+var cartaTobi = {
+  nome: "Tobi",
+  imagem: "https://nerdhits.com.br/wp-content/uploads/2021/04/tobi.jpg",
+  atributos: {
+    ataque: 14,
+    defesa: 8,
+    chakra: 11
+  }
+};
+
+var cartaJiraya = {
+  nome: "Jiraya",
+  imagem:
+    "https://i.pinimg.com/474x/d3/66/8e/d3668ebe29c430f5e17e6894343d92af--naruto-jiraiya-tsunade.jpg",
+  atributos: {
+    ataque: 17,
+    defesa: 11,
+    chakra: 13
+  }
+};
+
+var cartaKurenai = {
+  nome: "Kurenai",
+  imagem:
+    "https://pm1.narvii.com/6310/02d6dea3bcf24fddd54e510fbc8d494adb992d55_hq.jpg",
+  atributos: {
+    ataque: 12,
+    defesa: 11,
+    chakra: 18
+  }
+};
+
+var cartaMaquina;
+var cartaJogador;
+var cartas = [
+  cartaNaruto,
+  cartaSasuke,
+  cartaSakura,
+  cartaLee,
+  cartaNeji,
+  cartaHinata,
+  cartaGaara,
+  cartaTemari,
+  cartaTenten,
+  cartaIno,
+  cartaShikamaru,
+  cartaItachi,
+  cartaKakashi,
+  cartaTsunade,
+  cartaKisame,
+  cartaHidan,
+  cartaDeidara,
+  cartaKakuso,
+  cartaKonan,
+  cartaSasori,
+  cartaOrochimaru,
+  cartaTobi,
+  cartaJiraya,
+  cartaKurenai
+];
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 24);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * 24);
+  while (numeroCartaJogador == numeroCartaMaquina) {
+    numeroCartaJogador = parseInt(Math.random() * 24);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+    }
+  }
+}
+
+function jogar() {
+  console.log("chamou");
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Venceu</p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Perdeu</p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'>Empatou</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  //divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
